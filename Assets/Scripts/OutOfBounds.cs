bcf2a4c0ffcdca3dad83177d757bb017fb2dfaed
VanWoenselBram <92750755+VanWoenselBram@users.noreply.github.com>
2021-11-09 14:31:13 +0100

    Update OutOfBounds.cs

--- Assets/Scripts/OutOfBounds.cs
+++ Assets/Scripts/OutOfBounds.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class OutOfBounds : MonoBehaviour
 {
-    bool inBounds;
+    float Zkant1 = 965;
+    float Zkant2 = 760;
+    float Xkant1 = 270;
+    float Xkant2 = 65;
+
+    bool BuitenZ = false;
+    bool BuitenX = false;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameObject.transform.position.z > Zkant1 || gameObject.transform.position.z < Zkant2)
+        {
+            BuitenZ = true;
+        }
+        if (gameObject.transform.position.x > Xkant1 || gameObject.transform.position.x < Xkant2)
+        {
+            BuitenX = true;
+        }
+        if (BuitenX || BuitenZ)
+        {
+            Destroy(gameObject);
+        }
     }
-
 }
