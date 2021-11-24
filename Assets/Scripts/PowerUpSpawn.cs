7a4fc203e8134c61ee60baffd72881539be8f344
VanWoenselBram <92750755+VanWoenselBram@users.noreply.github.com>
2021-11-24 11:44:49 +0100

    .

--- Assets/Scripts/PowerUpSpawn.cs
+++ Assets/Scripts/PowerUpSpawn.cs
@@ -14,6 +14,7 @@ public class PowerUpSpawn : MonoBehaviour
     public GameObject[] PowUps;
 
     public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
