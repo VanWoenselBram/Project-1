c706d9b9c2cc384d68cb6a7aa8e3a2767ea5da2c
VanWoenselBram <92750755+VanWoenselBram@users.noreply.github.com>
2021-12-08 21:37:42 +0100

    EnemySchiet

---
+++ Assets/Scripts/EnemySchiet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Schiet : MonoBehaviour
+{
+    public bool PowerUp = false;
+
+    public Hp hp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PowerUp == true)
+        {
+            StartCoroutine(PoweredUp());
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            schiet();
+        }
+    }
+    void schiet()
+    {
+        RaycastHit schot;
+        Physics.Raycast(spelerCamera.transform.position, spelerCamera.transform.forward, out schot);
+        Collider collider = schot.collider;
+        if (schot.collider.CompareTag("Player"))
+        {
+            Scenemanager.loadscene("Game Over");
+        }
+    }
+}
